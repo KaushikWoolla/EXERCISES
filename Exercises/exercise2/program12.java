6691868c591c884aea6a46d6e8ee667dc7cac909
KaushikWoolla <55707246+KaushikWoolla@users.noreply.github.com>
2021-04-15 18:05:19 +0530

    Add files via upload

---
+++ Exercises/exercise2/program12.java
@@ -0,0 +1,11 @@
+package exercise2;
+
+public class program12 {
+	public static void main(String args[]) {
+		String hi = "Hello, ";
+		String mom = "child.";
+		String method1=Hello+child;
+		String method2=Hello.concat(child);
+	}
+ 
+}
