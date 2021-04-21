44a76c4078836ddde2d3973791409fbe1ed581d8
KaushikWoolla <55707246+KaushikWoolla@users.noreply.github.com>
2021-04-21 09:06:21 -0700

    Add files via upload

---
+++ Exercises/SERVLETS/SYSDATE&LOGINFORM/src/main/java/date.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Calendar;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class date extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public date() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		response.getWriter().print("<br>"+Calendar.getInstance().getTime());
+		}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
